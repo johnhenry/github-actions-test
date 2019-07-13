db134bbd0663e0af63a0f07d177a6ae635545f77
John Henry <john@iamjohnhenry.com>
2019-07-12 22:23:41 -0700

    use Dockerfile

---
+++ .github/action-a/entrypoint.sh
@@ -0,0 +1,9 @@
+OLD_VERSION=$(git describe --tags)
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+echo $OLD_VERSION
+echo $PACKAGE_VERSION
