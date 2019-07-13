f65bc80ae16d4c8f29856afcdc94a7703f99c9f1
John Henry <john@iamjohnhenry.com>
2019-07-12 22:40:03 -0700

    add exit code

--- .github/action-a/entrypoint.sh
+++ .github/action-a/entrypoint.sh
@@ -1,4 +1,4 @@
-OLD_VERSION=$(git describe --tags)
+OLD_VERSION=$(git describe --abbrev=0 --tags)
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
@@ -7,3 +7,4 @@ PACKAGE_VERSION=$(cat package.json \
   | tr -d '[[:space:]]')
 echo $OLD_VERSION
 echo $PACKAGE_VERSION
+exit 0
