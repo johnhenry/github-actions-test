c26d176680c06c2f7d9117d34bd7ba58de252f11
John Henry <john@iamjohnhenry.com>
2019-07-12 23:06:44 -0700

    stuff

--- .github/action-a/entrypoint.sh
+++ .github/action-a/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh -l
 
-sh -c "echo $*"
+OLD_VERSION=$(git describe --abbrev=0 --tags)
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+echo $OLD_VERSION
+echo $PACKAGE_VERSION
+exit 0
