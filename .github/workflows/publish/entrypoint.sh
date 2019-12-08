3aec0cc131cf1b8bbad5e75dc9436795fb6ef5f9
John Henry <john@iamjohnhenry.com>
2019-12-07 21:52:08 -0800

    use docker

--- .github/workflows/publish/entrypoint.sh
+++ .github/workflows/publish/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -l
 
+echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" >> ~/.npmrc
 
+git config --global user.name '(none)' && git config --global user.email 'noreply@github.com' && git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/$GITHUB_REPOSITORY
+npm install
+npm run build:dist
+npm run build:readme
+git add dist
+git add package-lock.json
+git add readme.md
+git commit -n -m "Built dist for $NEW_VERSION"
+echo "UPDATING VERSION: $VERSION => $NEW_VERSION ($UPDATE)"
+
+np $NEW_VERSION
+git push origin master
+git push --tags origin master
 
 exit 0
